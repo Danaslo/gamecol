c30c196ffee74617ca385b84d334f9368c328f4f
DanielAstorgaFA <daniel.astorga.lopez@iesfernandoaguilar.es>
2025-03-18 21:25:56 +0100

    Terminado el formulario de registro en Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// filepath: /Proyecto2/frontend/src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'registro', component: RegistroComponent },
+];
