26829a735e94d5187d50cf5e6131772c707229a6
danaslo <astorgalopezd89@gmail.com>
2026-03-24 07:57:10 +0100

    Despliegue del frontend

--- frontend/src/app/componentes/index/index.component.ts
+++ frontend/src/app/componentes/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,RouterModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
