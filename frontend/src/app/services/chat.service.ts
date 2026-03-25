08f439c1e9b84319eb564250b5fe7ad3acf0088e
danaslo <astorgalopezd89@gmail.com>
2026-03-25 20:20:54 +0100

    Despliegue: Corregiendo errores

--- frontend/src/app/services/chat.service.ts
+++ frontend/src/app/services/chat.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
